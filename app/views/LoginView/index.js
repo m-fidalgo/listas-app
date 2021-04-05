1f622acf6f1bc443a1de5dc4c457c5ed48f47d74
Maira Fidalgo <mairaberlatof@gmail.com>
2021-04-05 14:40:37 -0300

    Cadastro de novos usuários

--- app/views/LoginView/index.js
+++ app/views/LoginView/index.js
@@ -8,22 +8,25 @@ export default function LoginView(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  const auth = firebase.auth();
+  const [isNewUser, setIsNewUser] = useState(false);
 
   useEffect(() => {
-    const onAuthStateUnsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        props.onLogin(user);
-      }
-    });
+    const onAuthStateUnsubscribe = firebase
+      .auth()
+      .onAuthStateChanged((user) => {
+        if (user) {
+          props.onLogin(user);
+        }
+      });
 
-    return onAuthStateUnsubscribe();
-  }, []);
+    return () => onAuthStateUnsubscribe();
+  });
 
   async function login() {
     try {
-      const resp = await auth.signInWithEmailAndPassword(email, password);
-      props.onLogin(resp.user);
+      const resp = await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password);
     } catch (error) {
       setErrorMessage("Email e/ou Senha inválidos");
       setEmail("");
@@ -31,8 +34,30 @@ export default function LoginView(props) {
     }
   }
 
+  async function signInEmail() {
+    if (password.length >= 6) {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(email, password);
+      } catch (error) {
+        setErrorMessage("Erro no cadastro");
+        setEmail("");
+        setPassword("");
+      }
+    } else {
+      setErrorMessage("A senha deve ter no mínimo 6 caracteres");
+      setEmail("");
+      setPassword("");
+    }
+  }
+
   return (
     <View style={styles.container}>
+      <View>
+        <Button
+          title={isNewUser ? "Fazer login" : "Novo Cadastro"}
+          onPress={() => setIsNewUser(!isNewUser)}
+        />
+      </View>
       <View style={styles.loginContainer}>
         <TextInput
           style={styles.loginInput}
@@ -49,7 +74,11 @@ export default function LoginView(props) {
           value={password}
           onChangeText={(text) => setPassword(text)}
         />
-        <Button title="Login" onPress={login} />
+        {isNewUser ? (
+          <Button title="Cadastrar" onPress={signInEmail} />
+        ) : (
+          <Button title="Login" onPress={login} />
+        )}
         <Text style={styles.loginError}>{errorMessage}</Text>
       </View>
     </View>
@@ -67,6 +96,7 @@ const styles = StyleSheet.create({
     width: "90%",
     borderWidth: 2,
     padding: 15,
+    marginTop: 15,
   },
   loginInput: {
     fontSize: 18,
